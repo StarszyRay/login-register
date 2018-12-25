7af9c1f74674ed66e23fe43dc9d6f1b52386c25c
StarszyRay <staron321@gmail.com>
2018-12-25 18:25:41 +0100

    Added working key import

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,9 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
+import { cloack } from './myKeys';
 
-const myApiKey = config.MY_API_KEY;
+const myApiKey = cloack.MY_API_KEY;
 export const environment = {
   production: false,
   firebase: {
